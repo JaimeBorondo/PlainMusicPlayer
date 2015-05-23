4be20e69963476f37557d93caf8b11f126740227
Jaime Borondo <jaime.borondo@gmail.com>
2015-05-23 18:32:10 +0200

    Missing comitted file

--- PlainMusicPlayer/Song.cpp
+++ PlainMusicPlayer/Song.cpp
@@ -67,68 +67,6 @@ unsigned Song::GetLength()
     return info_.get_length();
 }
 
-//TODO, Separate these into files per class
-
-#include <tbytevectorstream.h>
-
-SongInfo::SongInfo(){}
-
-SongInfo::SongInfo(const std::wstring &filename) : filename_(filename)
-{
-    TagLib::FileRef f(filename.c_str());
-    album_ = f.tag()->album().toWString().data();
-    artist_ = f.tag()->artist().toWString().data();
-    title_ = f.tag()->title().toWString().data();
-
-    bitrate_ = f.audioProperties()->bitrate();
-    frequency_ = f.audioProperties()->sampleRate();
-    length_ = f.audioProperties()->length();
-    stereo_ = f.audioProperties()->channels() > 1;
-}
-
-SongInfo::~SongInfo(){}
-
-const std::wstring &SongInfo::get_filename() const
-{
-    return filename_;
-}
-
-const std::wstring &SongInfo::get_title() const
-{
-    return title_;
-}
-
-const std::wstring &SongInfo::get_artist() const
-{
-    return artist_;
-}
-
-const std::wstring &SongInfo::get_album() const
-{
-    return album_;
-}
-
-const unsigned &SongInfo::get_length() const
-{
-    return length_;
-}
-
-const unsigned &SongInfo::get_bitrate() const
-{
-    return bitrate_;
-}
-
-const unsigned &SongInfo::get_frequency() const
-{
-    return frequency_;
-}
-
-bool SongInfo::is_stereo() const
-{
-    return stereo_;
-}
-
-
 std::wstring Song::GetDisplayName()
 {
     return info_.get_artist() + L" - " + info_.get_title() + L" (" + info_.get_album() + L")";
